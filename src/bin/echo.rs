f459119d6a3521f5d64c10c9aa371d7c5ab4214f
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-04-14 18:53:53 +0530

    echo node

---
+++ src/bin/echo.rs
@@ -0,0 +1,77 @@
+use distributed_systems::*;
+
+use anyhow::{bail, Context};
+use serde::{Deserialize, Serialize};
+use std::io::{StdoutLock, Write};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo {
+        echo: String,
+    },
+    EchoOk {
+        echo: String,
+    },
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk,
+}
+
+// State Machine for the ECHO Node of the Distributed System
+struct EchoNode {
+    id: usize,
+}
+
+impl Node<Payload> for EchoNode {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
+        match input.body.payload {
+            Payload::Init { .. } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::InitOk,
+                    },
+                };
+
+                //Serialize Message as JSON into the I/O stream.
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to init")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+
+                self.id += 1;
+            }
+
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to init")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+                self.id += 1;
+            }
+            Payload::EchoOk { .. } => bail!("recieved init_ok message"),
+            Payload::InitOk { .. } => {}
+        };
+
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop(EchoNode { id: 0 })
+}
